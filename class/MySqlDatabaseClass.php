8c79e9dc5ddfc6b0126afe896a6ce02ba7247741
Amin Razzak <amin.hybrid77@gmail.com>
2013-11-26 11:48:37 +0100

    hier heb ik een config en mysqldatabase class gemaakt

--- class/MySqlDatabaseClass.php
+++ class/MySqlDatabaseClass.php
@@ -1,3 +1,22 @@
 <?php
+	class MySqlDatabaseClass{
+		
+		//fields
+		private $db_connection;
+	
 
+
+	/*dit is de constructor van de MySqlDatabaseClass
+	*de constructor herken je in php aan het feit dat
+	*de naam construct is 
+	*/
+	
+	public function __construct(){
+	// Velden roep je aan met $this->naam van het veld zonder $teken er voor.	
+	$this->db_connection = mysql_connect(SERVERNAME,
+										USERNAME,
+										PASSWORD);
+		
+	}
+}
 ?>
---
+++ config/config.php
@@ -0,0 +1,19 @@
+<?php
+	$userrole = array('customer','root');
+	include ("security.php"); 
+?>
+
+<h3> Adminpage <h3/>
+
+
+<?php
+
+	define('SERVERNAME', 'localhost');
+	
+	define('USERNAME', 'root');
+	define('PASSWORD', '');
+	define('DATABASE', 'fotosjaak');
+	date_default_timezone_set("Europe/Amsterdam");
+	
+	
+?>
