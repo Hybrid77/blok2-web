930547268d126e9b1f1f06c5279ff9191172e7ef
Amin Razzak <amin.hybrid77@gmail.com>
2014-01-14 09:39:59 +0100

    we hebben hier bij de opdracht pagina twee radio buttons toegevoegt en omschrijving hokjes

--- activation.php
+++ activation.php
@@ -1,77 +1,72 @@
 <?php
-require_once ("class/LoginClass.php");
+require_once("class/LoginClass.php");
 
-        if (isset($_POST['submit']))
+if (isset($_POST['submit']))
+{
+        echo "Er is op de submitknop gedrukt!";        
+        if (!strcmp($_POST['password'], $_POST['password-check']))
         {
-                if(!strcmp($_POST['password'], $_POST['password-check']))
-                {
-                        // Sla het nieuwe wachtwoord op in de database
-                        LoginClass::update_password_in_login($_POST['email'], $_POST['password']);
-                        echo "Uw wachtwoord is succesvol gewijzigd, u wordt doorgestuurd naar de login pagina";
-                        header("refresh:5;url=index.php?content=login_form");
-                }
-                else 
-                {
-                        echo "De wachtwoorden komen niet overeen, u wordt teruggestuurd naar de wijzigingspagina";
-                        header("refresh:5;url=index.php?content=activation&email=".$_POST['email']."&password=".$_POST['oldpassword']);        
-                }
+                // Sla het nieuwe wachtwoord op, in de tabel login
+                LoginClass::update_password_in_login($_POST['email'],
+                                                                                         $_POST['password']);
+                echo "Uw wachtwoord is succesvol gewijzigd. U wordt doorgestuurd
+                          naar de inlogpagina";
+                header("refresh:5;url=index.php?content=login_form");                
         }
         else 
         {
-                
-                if (LoginClass::check_if_email_password_exists($_GET['email'], $_GET['password']))
-                {
-                ?>
-                        <p>Welkom op de site, uw account wordt geactiveerd nadat u een nieuw wachtwoord heeft gekozen.</p>
-                        
-                                <form action='index.php?content=activation' method='post'>
-                                        <table>
-                                                <tr>
-                                                        <td>Nieuw wachtwoord(max. 12 tekens)</td>
-                                                        
-                                                        <td><input type="password" 
-                                                                           name="password" 
-                                                                           size="12" 
-                                                                           maxlength="12" />
-                                                        </td>
-                                                </tr>
-                                                
-                                                <tr>
-                                                        <td>Nieuw wachtwoord(check)</td>
-                                                        
-                                                        <td><input type="password" 
-                                                                       name="password-check" 
-                                                                       size="12"
-                                                                       maxlength="12"/>
-                                                        </td>
-                                                </tr>
-                                                
-                                                <tr>
-                                                        <td>&nbsp;</td>
-                                                        
-                                                        <td><input type="submit" 
-                                                                       value="Verstuur"
-                                                                       name="submit" />
-                                                                       <input type="hidden"
-                                                                                     name="email"
-                                                                                         value='<?php echo $_GET['email']; ?>'
-                                                                                          />
-                                                                                          
-                                                                       <input type="hidden"
-                                                                              name="oldpassword"
-                                                                              value='<?php echo $_GET['password']; ?>' 
-                                                                              />                   
-                                                        </td>
-                                                </tr>
-                                        </table>
-                                        
-                                </form>
-                        <?php
-                }
-                else 
-                {
-                        echo "U heeft geen rechten op deze pagina, u wordt doorgestuurd naar de homepage.";        
-                        header("refresh:4;url=index.php?content=homepage");
-                }
+                echo "De ingevoerde wachtwoorden komen niet overeen,<br>
+                          probeer het opnieuw. U wordt teruggestuurd";
+                header("refresh:5;url=index.php?content=activation&email=".$_POST['email']."&password=".$_POST['old_password']);
         }
+}
+else 
+{
+        if (LoginClass::check_if_email_password_exists($_GET['email'], $_GET['password']))
+        {
+        ?>
+        <p>Welkom op de site. Uw account wordt geactiveerd nadat<br> u een nieuw wachtwoord heeft gekozen</p>
+        <form action='index.php?content=activation' method='post'>
+                <table>
+                        <tr>
+                                <td>nieuw wachtwoord(maximaal 12 tekens)</td>
+                                <td>
+                                        <input type='password'
+                                                   name='password'
+                                                   size='12'
+                                                   maxlength='12'/>
+                                </td>
+                        </tr>
+                        <tr>
+                                <td>nieuw wachtwoord (check)</td>
+                                <td><input type='password' 
+                                                   name='password-check' 
+                                                   size='12' 
+                                                   maxlength='12' />
+                                </td>
+                        </tr>
+                        <tr>
+                                <td>&nbsp;</td>
+                                <td>
+                                        <input type='submit' value='verstuur' name='submit' />
+                                        <input type='hidden'
+                                                   name='email' 
+                                                   value='<?php echo $_GET['email']; ?>' />
+                                        <input type='hidden'
+                                                   name='old_password' 
+                                                   value='<?php echo $_GET['password']; ?>' />
+                                                                
+                                </td>
+                        </tr>
+                        
+                </table>        
+        </form>
+        <?php
+        }
+        else 
+        {
+                echo "U heeft geen rechten op deze pagina. U wordt doorgestuurd naar de homepage";
+                header("refresh:4;url=index.php?content=homepage");
+        }
+}
 ?>
