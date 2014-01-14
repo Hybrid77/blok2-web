58682b5627489adeebd9a4efaa23a8a73a39745f
Amin Razzak <amin.hybrid77@gmail.com>
2014-01-14 21:19:03 +0100

    hier hebben we de order class gemaakt zodat alle gegevens gestuurd worden naar de order tabel in onze myphpadmin

--- opdracht.php
+++ opdracht.php
@@ -1,11 +1,29 @@
 <?php 
         $userrole = array('customer', 'root', 'admin');
         include("security.php"); 
+
+
+		if ( isset($_POST['submit']))
+		{
+			//var_dump($_POST);
+			
+			echo "Uw geplaatste opdracht is correct ontvangen. U krijgt een<br>
+				 bevestigingsemail toegestuurd. U word binne 8 seconde terug gestuurd";
+				 header("refresh:6;indext.php?content=customer_homepage");
+		}
+		
+		else
+		{
+				
+		
 ?>
+
+
+
 <p>Plaats een opdracht</p>
 
 
-<form action='' method='post' >
+<form action='index.php?content=opdracht' method='post' >
         <table>
                 <tr>
                         <td>Korte omschrijving</td>
@@ -54,9 +72,20 @@
                 							 value='black-white'/>
                 		
                 	</td>
-                	
+                 </tr>
+                 
+                 <tr>
+                 	<td>Aantal foto's</td>
+                 	<td>
+                 		<input type='number' name='number_of_pictures' min='5' value='5'/>
+                 	</td>
+                 </tr>
+                <tr>
+                	<td>&nbsp</td>
+                	<td>
+                		<input type='submit' name='submit' value='verstuur' />
+                	</td>
                 </tr>
-                
                 
                 
                 
@@ -65,3 +94,7 @@
         
         
 </form>
+
+<?php
+}
+?>
