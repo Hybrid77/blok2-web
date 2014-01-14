841fd3418221f8cbe94a7aabd543b2b9c7f1afe3
Amin Razzak <amin.hybrid77@gmail.com>
2014-01-14 08:56:19 +0100

    was vergeten om een commit gisteren te maken maar we hebben ervoor gezorgd dat jij je ww kan wijzigen en dat je dat kon zien in php my admin

--- activation.php
+++ activation.php
@@ -1,43 +1,77 @@
-<p> Welkom, uw account word geactiveerd nadat u een nieuw wachtwoord heeft gekozen.</p>
+<?php
+require_once ("class/LoginClass.php");
 
-<from action='' method='post'>
-	<table>
-		<tr>
-			<td>nieuw wachtwoord <br>
-				<font size='0.1'>(maximaal 12 tekens)</font>
-			</td>
-		 	
-		 	
-		 	<td>
-		 		<input  type='text' 
-		 				name='password'
-		 				size='12'
-		 				maxlength='12' />
-		 	</td>
-		</tr>
-		
-		<tr>
-			<td>nieuw wachtwoord <br>
-				<font size='0.1'>(check)</font>
-			</td>		
-			
-			<td>
-				<input  type='text' 
-		 				name='password-check'
-		 				size='12'
-		 				maxlength='12'
-		 	</td>
-		</tr>
-		
-		<tr>
-			<td>&nbsp;</td>
-			<td>
-				<input  type='submit' 
-		 				name='submit'
-			</td>	
-			
-			
-		</tr>
-		
-	</table>
-</from> 
+        if (isset($_POST['submit']))
+        {
+                if(!strcmp($_POST['password'], $_POST['password-check']))
+                {
+                        // Sla het nieuwe wachtwoord op in de database
+                        LoginClass::update_password_in_login($_POST['email'], $_POST['password']);
+                        echo "Uw wachtwoord is succesvol gewijzigd, u wordt doorgestuurd naar de login pagina";
+                        header("refresh:5;url=index.php?content=login_form");
+                }
+                else 
+                {
+                        echo "De wachtwoorden komen niet overeen, u wordt teruggestuurd naar de wijzigingspagina";
+                        header("refresh:5;url=index.php?content=activation&email=".$_POST['email']."&password=".$_POST['oldpassword']);        
+                }
+        }
+        else 
+        {
+                
+                if (LoginClass::check_if_email_password_exists($_GET['email'], $_GET['password']))
+                {
+                ?>
+                        <p>Welkom op de site, uw account wordt geactiveerd nadat u een nieuw wachtwoord heeft gekozen.</p>
+                        
+                                <form action='index.php?content=activation' method='post'>
+                                        <table>
+                                                <tr>
+                                                        <td>Nieuw wachtwoord(max. 12 tekens)</td>
+                                                        
+                                                        <td><input type="password" 
+                                                                           name="password" 
+                                                                           size="12" 
+                                                                           maxlength="12" />
+                                                        </td>
+                                                </tr>
+                                                
+                                                <tr>
+                                                        <td>Nieuw wachtwoord(check)</td>
+                                                        
+                                                        <td><input type="password" 
+                                                                       name="password-check" 
+                                                                       size="12"
+                                                                       maxlength="12"/>
+                                                        </td>
+                                                </tr>
+                                                
+                                                <tr>
+                                                        <td>&nbsp;</td>
+                                                        
+                                                        <td><input type="submit" 
+                                                                       value="Verstuur"
+                                                                       name="submit" />
+                                                                       <input type="hidden"
+                                                                                     name="email"
+                                                                                         value='<?php echo $_GET['email']; ?>'
+                                                                                          />
+                                                                                          
+                                                                       <input type="hidden"
+                                                                              name="oldpassword"
+                                                                              value='<?php echo $_GET['password']; ?>' 
+                                                                              />                   
+                                                        </td>
+                                                </tr>
+                                        </table>
+                                        
+                                </form>
+                        <?php
+                }
+                else 
+                {
+                        echo "U heeft geen rechten op deze pagina, u wordt doorgestuurd naar de homepage.";        
+                        header("refresh:4;url=index.php?content=homepage");
+                }
+        }
+?>
