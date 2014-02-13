d6065fd5108835c90d494e7ba49e5d1ded119f1f
Amin Razzak <amin.hybrid77@gmail.com>
2014-02-13 11:58:46 +0100

    wat gespeelt met jquery

--- Fotosjaak toets/selectors.php
+++ Fotosjaak toets/selectors.php
@@ -1,1 +1,22 @@
-ss
+<script style='text/javascript'>
+	$("document").ready(function()
+	{
+		alert("WAZAAAAAAAA");		
+		
+		
+		
+	
+	
+	
+		$("#toggle").click(function()
+		{
+			$("p").toggle();
+			$("#toggle").text('toggle to show');
+		});
+		
+	
+	});
+</script>
+<p id ='eerste'>Dit is de eerste paragraaf</p>
+<p id ='tweede'>Dit is de tweede paragraaf</p>
+<button id='toggle'>toggle to hide</button>
